131f6cf74234a6e365d95cb74b6bccc499ab092f
Ethanol10 <35375774+Ethanol10@users.noreply.github.com>
2021-07-29 22:16:04 +1000

    updated the parser
    uses () and = of both half and fullwidth for parsing

--- chatCommands/furiganaWriter.js
+++ chatCommands/furiganaWriter.js
@@ -4,60 +4,127 @@ const botConfig = require('../config.json');
 module.exports = {
     furiganaize: async function(message){
         //formatting for this message:
-        //$furiganaize (bottom text){top text}(bottom text){top text} 
-
-        rawMessageContent = message.content.substring(botConfig.prefix + "furiganaize ");
+        //^furiganaize （僕＝ぼく）の（最弱＝さいきょう）を（以＝も）って、（君＝きみ）の（最強＝さいきょう）を（打＝う）ち（破＝やぶ）
+        var prefix = botConfig.prefix + "furiganaize ";
+        rawMessageContent = message.content.substring(prefix.length);
+        //message.channel.send(rawMessageContent);
 
         //text parsing:
-        //check for a bracket, pool all rb text into rb field, then end on last bracket.
-        //check for a curlybrace, pool all rt text into an rt field, then end on last curlybrace.
         var textObjects = [];
         var object = {
             rb: "",
             rt: ""
         }
         var rbWriting = false;
-        var rtWriting = false;
+        var parenthesisFlag = false;
+        var equalFlag = false;
         for(var i = 0; i < rawMessageContent.length; i++){
-            if(rawMessageContent.charAt(i) === "(" || rawMessageContent.charAt(i) === "（" ){
+            if(rawMessageContent.charAt(i) === "(" || rawMessageContent.charAt(i) === "（"){
+                if(object.rb !== ""){
+                    textObjects.push({...object});
+                    object.rb = "";
+                    object.rt = "";
+                }
+                parenthesisFlag = true;
                 rbWriting = true;
-                i++;
+            }
+            if(rawMessageContent.charAt(i) === "=" || rawMessageContent.charAt(i) === "＝"){
+                equalFlag = true;
+                rbWriting = false;
             }
             if(rawMessageContent.charAt(i) === ")" || rawMessageContent.charAt(i) === "）" ){
-                rbWriting = false;
-                i++
+                rbWriting = true;
+                parenthesisFlag = true;
+                //Reset object
+                textObjects.push({...object});
+                object.rb = "";
+                object.rt = "";
+            }
+            if(!parenthesisFlag && !equalFlag){
+                if(rbWriting){
+                    if(rawMessageContent.charAt(i) === "\n"){
+                        console.log("linebreakrb!");
+                        object.rb += "<br/>";
+                    }
+                    else{
+                        object.rb += rawMessageContent.charAt(i);
+                    }
+                }
+                else if(!rbWriting){
+                    object.rt += rawMessageContent.charAt(i)
+                }
             }
 
-
-            if(converting){
-                if(!rbDone){
-                    rb += rawMessageContent.charAt(i);
-                }
-                else{
-                    rt += rawMessageContent.charAt(i);
-                }
-            }
+            parenthesisFlag = false;
+            equalFlag = false;
+        }
+        if(object.rb !== ""){
+            textObjects.push({...object});
         }
         
-        console.log("rb: " + rb);
-        console.log("rt: " + rt);
+        var rbLength = 0;
+        var rtLength = 0;
+        var ruby = "<ruby>\n";
 
+        //Following this format.
+        /*
+        <ruby>`+ 
+            ruby +`<rp>(</rp><rt>` + rubyrt + `</rt><rp>)</rp>
+        </ruby></br>
+        <ruby>`+ 
+            ruby +`<rp>(</rp><rt>` + rubyrt + `</rt><rp>)</rp>
+        </ruby>
+        */
+        for(var i = 0; i < textObjects.length; i++){
+            //message.channel.send("bottom: " + textObjects[i].rb + " top: " + textObjects[i].rt);
+            ruby += textObjects[i].rb + "<rp>(</rp><rt>" + textObjects[i].rt + "</rt><rp>)</rp>\n"
+            if(textObjects[i].rb.includes("<br/>")){
+                ruby+= "</ruby><br/>\n<ruby>";
+            }
+            rbLength += textObjects[i].rb.length;
+            rtLength += textObjects[i].rt.length;
+        }
+
+        ruby += "</ruby>"
+        //calculate whether the maxwidth is exceeded
+        var widthOp;
+        var multiplier = 45;
+        var maxWidth = 900;
+        if(rbLength > rtLength){
+            if(rbLength * multiplier > maxWidth){
+                widthOp = maxWidth;
+            }
+            else {
+                widthOp = rbLength * multiplier;
+            }
+        } else{
+            if(rtLength * multiplier > maxWidth){
+                widthOp = maxWidth;
+            }
+            else{
+                widthOp = rtLength * multiplier;
+            }
+        }
+
+        //gather up all options
         var options = {
-            windowSize: {width: (rb.length > rt.length) ? rb.length * 40 : rt.length * 40, height: 50},
+            windowSize: {width: widthOp, height: 50},
             shotSize: {width: 'all', height: 'all'},
             quality: 100,
             streamType: 'png',
             siteType: 'html',
             defaultWhiteBackground: false
         };
+        
+        //message.channel.send(ruby);
 
-        await setImg(rb, rt, options);        
+        await setImg(ruby, options);        
 
         message.channel.send({files: ["./imgStore/furiganaImg.png"]}); 
     }
 }
 
-async function setImg(ruby, rubyrt, options){
+async function setImg(ruby, options){
     return new Promise( (resolve, reject) => {
         webshot(
             `<html>
@@ -71,9 +138,9 @@ async function setImg(ruby, rubyrt, options){
                 <body>
                     <text>
                         <div id="container">
-                            <ruby>`+ 
-                                ruby +`<rp>(</rp><rt>` + rubyrt + `</rt><rp>)</rp>
-                            </ruby>
+                            `
+                            + ruby +
+                            `
                         </div>
                     </text>
                 </body>
